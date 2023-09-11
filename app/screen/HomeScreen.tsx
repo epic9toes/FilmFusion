fe13a4f787a9ec25b7fac8a11f1e20e7e8c08665
epic9toes <kr8tiveonwe@gmail.com>
2023-09-11 20:55:32 +0100

    navigation added

---
+++ app/screen/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>HomeScreen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
